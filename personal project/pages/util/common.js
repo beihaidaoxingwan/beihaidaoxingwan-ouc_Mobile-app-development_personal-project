3a03d0c7c010a26edf98292a3ac809cc19eb32f3
beihaidaoxingwan <84618650+beihaidaoxingwan@users.noreply.github.com>
2022-09-04 14:21:23 +0800

    Add files via upload

---
+++ personal project/pages/util/common.js
@@ -0,0 +1,133 @@
+
+const person=[{
+    id:1,
+    name:'李白',
+    date:'701年—762年12月',
+    place:'山东',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.cdd6a89f24527c242061088967ade23c?rik=0S8wjp6vo3D3ew&riu=http%3a%2f%2fwww.askququ.com%2fuploads%2fallimg%2f200126%2f1_200126212832_1.jpg&ehk=s9kqLYNEYZ6sUXi%2bXugFkxCy%2b%2bYuJJgs6heq2blHRjE%3d&risl=&pid=ImgRaw&r=0',
+    introduce:'李白（701年—762年12月），字太白，号青莲居士，又号“谪仙人”，唐代伟大的浪漫主义诗人，被后人誉为“诗仙”，与杜甫并称为“李杜”，为了与另两位诗人李商隐与杜牧即“小李杜”区别，杜甫与李白又合称“大李杜”。《旧唐书》记载李白为山东人 ；《新唐书》记载，李白为兴圣皇帝李暠九世孙，与李唐诸王同宗。其人爽朗大方，爱饮酒作诗，喜交友 。',
+    work1:'《望庐山瀑布》',
+    work2:'《行路难》',
+    work3:'《将进酒》'
+  },
+  {
+    id:2,
+    name:'李贺',
+    date:'790年—816年',
+    place:'陇西郡',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.05b7c43a53ea1b1e798ce5c20a09bcc3?rik=ttaf6tzwVwxXkQ&riu=http%3a%2f%2fp.jingyanben.com%2f0612%2f0d6e0ea5e1f49.png&ehk=b2k9GOSLQ07sq2%2bDVg70WGwaEBtzF%2bAHnwkWhgkLk90%3d&risl=&pid=ImgRaw&r=0',
+    introduce:'李贺（790年—816年），字长吉。河南府福昌县昌谷乡（今河南省宜阳县）人，祖籍陇西郡。唐朝中期浪漫主义诗人，与诗仙李白、李商隐称为“唐代三李”，后世称李昌谷。李贺出身唐朝宗室大郑王（李亮）房，门荫入仕，授奉礼郎。仕途不顺，热心于诗歌创作。作品慨叹生不逢时、内心苦闷， 抒发对理想抱负的追求，反映藩镇割据、宦官专权和社会剥削的历史画面。诗作想象极为丰富，引用神话传说，托古寓今，后人誉为“诗鬼”。27岁（一说24岁）英年早逝。',
+    work1:'《高轩过》',
+    work2:'《雁门太守行》',
+    work3:'《李凭箜篌引》'
+  },
+  {
+    id:3,
+    name:'屈原',
+    date:'约公元前340—公元前278年',
+    place:'楚国丹阳秭归',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.1ba9059cb6b7606ec49710a6df3fdc58?rik=8g2r%2bVtnUSzWBQ&riu=http%3a%2f%2fpic.616pic.com%2fys_bnew_img%2f00%2f01%2f93%2fTwceJ3h5p0.jpg&ehk=4E89qhdH3s2qKvsl9EugiR%2fDUcdzmss1DTyX0LoEeiQ%3d&risl=&pid=ImgRaw&r=0',
+    introduce:'屈原（约公元前340—公元前278年），芈姓，屈氏，名平，字原，又自云名正则，字灵均，出生于楚国丹阳秭归（今湖北宜昌），战国时期楚国诗人、政治家。楚武王熊通之子屈瑕的后代。少年时受过良好的教育，博闻强识，志向远大。早年受楚怀王信任，任左徒、三闾大夫，兼管内政外交大事。提倡“美政”，主张对内举贤任能，修明法度，对外力主联齐抗秦。因遭贵族排挤诽谤，被先后流放至汉北和沅湘流域。 楚国郢都被秦军攻破后，自沉于汨罗江，以身殉楚国。',
+    work1:'《离骚》',
+    work2:'《九歌》',
+    work3:'《天问》'
+  },
+  {
+    id:4,
+    name:'李商隐',
+    date:'约813年～约858年',
+    place:'怀州河内',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.1a0569a29c5e1e6eddc36cfb3e33ff8b?rik=QLQiNB4wEfU5mw&riu=http%3a%2f%2fpic.baike.soso.com%2fp%2f20120311%2f20120311164630-721824092.jpg&ehk=bQjia7vGGEDKE1pT7cLx4qSrnLwmYQr7Qpm5731vOWk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    introduce:'李商隐（约813年～约858年），字义山，号玉谿生，怀州河内（今河南省沁阳市）人。晚唐著名诗人，和杜牧合称“小李杜”。开成二年（837年），进士及第，起家秘书省校书郎，迁弘农县尉，成为泾原节度使王茂元（岳父）幕僚。卷入“牛李党争”的政治旋涡，备受排挤，一生困顿不得志。大中末年，病逝于郑州。',
+    work1:'《无题》',
+    work2:'《锦瑟》',
+    work3:'《夜雨寄北》'
+  },
+  {
+    id:5,
+    name:'宋玉',
+    date:'公元前298年—公元前222年',
+    place:'楚国',
+    poster:'https://p1.ssl.qhimg.com/t01c71de6b08e54e533.png',
+    introduce:'宋玉（公元前298年—公元前222年），楚国诗人，宋国公族后裔，生于楚国，曾担任楚顷襄王，为楚国士大夫。 战国著名辞赋家，宋玉与唐勒，景差齐名，传世作品有《九辩》等。所谓“下里巴人”、“阳春白雪”、“曲高和寡”便说的是他，这些典故皆他而来。始皇帝二十五年己卯，公元前222年因病去世，享年七十六岁。',
+    work1:'《九辩》',
+    work2:'《笛赋》',
+    work3:'《高唐对》'
+  },
+  {
+    id:11,
+    name:'威廉·华兹华斯',
+    date:'1770-1850年',
+    place:'英国',
+    poster:'https://bkimg.cdn.bcebos.com/pic/7aec54e736d12f2e88fbd63b4bc2d562843568ea?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5/format,f_auto',
+    introduce:'华兹华斯（William Wordsworth，1770-1850年），英国浪漫主义诗人，曾当上桂冠诗人。其诗歌理论动摇了英国古典主义诗学的统治，有力地推动了英国诗歌的革新和浪漫主义运动的发展。他是文艺复兴运动以来最重要的英语诗人之一 ，其诗句“朴素生活，高尚思考（plain living and high thinking）”被作为牛津大学基布尔学院的格言.',
+    work1:'《抒情歌谣集》',
+    work2:'《序曲》',
+    work3:'《露西》'
+  },
+  {
+    id:12,
+    name:'塞缪尔·泰勒·柯勒律治',
+    date:'1772.10.21－1834.7.25',
+    place:'英国',
+    poster:'https://p1.ssl.qhimg.com/dr/270_500_/t01d9ae7b6139191b58.jpg?size=300x400',
+    introduce:'塞缪尔·泰勒·柯勒律治（Samuel Taylor Coleridge，1772年10月21日－1834年7月25日），英国诗人、文评家，英国浪漫主义文学的奠基人之一。他以〈古舟子咏〉(亦可称作〈古舟子之歌〉)(The Rime of the Ancient Mariner)一诗名家，其文评集《文学传记》(Biographia Literaria)以博大精深见称，书中对想像(imagination)与幻想(fancy)的区别尤其著名。 一生作诗不缀，但中年时自称弃诗从哲，精研以康德、谢林为首的德国唯心论。他的“鸦片瘾”、他的个人魅力、他与华兹华斯的微妙关系，使他成为西方文学史上最令人注目的作家之一。',
+    work1:'《古舟子咏》',
+    work2:'《克里斯塔贝尔》',
+    work3:'《忽必烈汗》'
+  },
+  {
+    id:13,
+    name:'夏多布里昂',
+    date:'1768～1848',
+    place:'法国',
+    poster:'https://file.mingyantong.com/category_pictures/201507/xiaduobuliangjingdianyulu2262.jpg',
+    introduce:'夏多布里昂（1768～1848），法国十八至十九世纪的作家，政治家，外交家，法兰西学院院士。1768年9月4日生于法国布列塔尼伊勒-维莱讷省的圣马洛市，1848年7月4日卒于巴黎。拿破仑时期曾任驻罗马使馆秘书，波旁王朝复辟后成为贵族院议员，先后担任驻瑞典和德国的外交官，及驻英国大使，并于1823年出任外交大臣。著有小说《阿达拉》《勒内》《基督教真谛》，长篇自传《墓畔回忆录》等，是法国早期浪漫主义的代表作家。',
+    work1:'《阿达拉》',
+    work2:'《基督教真谛》',
+    work3:'《墓畔回忆录》'
+  },
+  {
+    id:14,
+    name:'维克多·雨果',
+    date:'1802.2.26—1885.5.22',
+    place:'法国',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.33f108a385f31617c06b2f54d6cbe6f9?rik=aETubfm4tyW8oQ&riu=http%3a%2f%2fwww.leewiart.com%2fuserfiles%2f22136%2ffad11da13c4247a8859266f81d38ee55_view_720.jpg%3f637568848699524753&ehk=8PpOrmUG7uhlA3wm1ZCOtUIrm%2fzEpStC9rwxXqYR1XE%3d&risl=&pid=ImgRaw&r=0',
+    introduce:'维克多·雨果（ 英文名：Victor Hugo，1802年2月26日—1885年5月22日），法国19世纪前期积极浪漫主义文学的代表作家，人道主义的代表人物，法国文学史上卓越的资产阶级民主作家，被人们称为“法兰西的莎士比亚”。一生写过多部诗歌、小说、剧本、各种散文和文艺评论及政论文章，在法国及世界有着广泛的影响力。1802年2月26日，雨果生于法国贝桑松，上有兄长二人。13岁时与兄长进入寄读学校就学，兄弟均成为学生领袖。他在16岁时已能创作杰出的诗句，21岁时出版诗集，声名大噪。1845年，法王路易·菲利普授予雨果上议院议员职位，自此专心从政。1848年法国二月革命爆发，法王路易被逊位。雨果于此时期四处奔走宣传革命，为人民贡献良多，赢得新共和政体的尊敬，晋封伯爵，并当选国民代表及国会议员。三年后，拿破仑三世称帝，他对此大加攻击，因此被放逐国外。此后20年间各处漂泊，此时期完成小说《悲惨世界》。1870年法国恢复共和政体（法兰西第三共和国），雨果结束流亡生涯，回到法国。1885年5月22日，雨果辞世，于潘德拉举行国葬。',
+    work1:'《巴黎圣母院》',
+    work2:'《悲惨世界》',
+    work3:'《九三年》'
+  },
+  {
+    id:15,
+    name:'海因里希·海涅',
+    date:'1797.12.13～1856.2.17',
+    place:'德国',
+    poster:'https://ts1.cn.mm.bing.net/th/id/R-C.5aa25625c3c06682769c0398d3a2492d?rik=g3N%2bzM4l5CLgQg&riu=http%3a%2f%2fimg.zwbk.org%2fbaike%2fspic%2f2010%2f11%2f16%2f20101116034500903_4442.jpg&ehk=YtqJviahHnSBIF%2bdcfdZj44n1FK%2bmjQbpLiieXQMs%2bA%3d&risl=&pid=ImgRaw&r=0',
+    introduce:'海因里希·海涅（Heinrich Heine，1797年12月13日～1856年2月17日），男，德国抒情诗人和散文家，被称为“德国古典文学的最后一位代表”。生于德国莱茵河畔杜塞尔多夫一个犹太人家庭。童年和少年时期经历了拿破仑战争。学过金融和法律。1795年，拿破仑的军队曾开进莱茵河流域，对德国的封建制度进行了一些民主改革。正如恩格斯所指出，拿破仑“在德国是革命的代表，是革命原理的传播者，是旧的封建社会的摧残人”。法军的这些改革，使备受歧视的犹太人的社会地位得到了较全面改善，因此海涅从童年起就接受了法国资产阶级革命思想的影响。 1821年开始发表诗作，以4卷《游记》（1826-27,1830-31）和《歌集》（1827）而闻名文坛。1825年为取得德国公民权而皈依基督教，但因此疏远了自己的犹太民族。而他的革命思想又使他在德国无法找到工作。1830年革命后自愿流亡巴黎，从诗歌写作转向政治活动，成为国家民主运动的领导人，同时对法国和德国文化有许多评述。1856年2月17日，海因里希·海涅在巴黎逝世。',
+    work1:'《罗曼采罗》',
+    work2:'《佛罗伦萨之夜》',
+    work3:'《游记》'
+  },
+]
+
+function getpersonDetail(personID){
+  let message = {
+   code:'404',
+    person:{}
+  }
+  for(var i = 0; i < person.length; i++){
+    //当新闻id匹配到数据库里的ID时便更新新闻（message）
+    if(personID == person[i].id){
+      message.code = '200';
+      message.person = person[i];
+      return message;
+      break;
+    }
+  }
+}
+//暴露接口
+module.exports = {
+  getpersonDetail:getpersonDetail
+}
+
